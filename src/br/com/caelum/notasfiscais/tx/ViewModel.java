d3dcd2df0f2e67707dd13a89009aa5969bc2b208
Marcus Perusso <marcusperusso@gmail.com>
2014-08-06 19:20:06 -0300

    teste

---
+++ src/br/com/caelum/notasfiscais/tx/ViewModel.java
@@ -0,0 +1,12 @@
+package br.com.caelum.notasfiscais.tx;
+
+import javax.enterprise.inject.Stereotype;
+import javax.inject.Named;
+
+
+@Named
+@javax.faces.view.ViewScoped
+@Stereotype
+public @interface ViewModel {
+
+}
